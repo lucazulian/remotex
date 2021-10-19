91e8465cff10e86e230ca62c23aa6a8ab91ff444
lucazulian <lucagiuggia@gmail.com>
2021-10-19 23:32:14 +0200

    ci: add dependabot and basic ci with tests and static analisys

--- mix.exs
+++ mix.exs
@@ -10,7 +10,10 @@ defmodule Remotex.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer(),
+      preferred_cli_env: preferred_cli_env(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -42,7 +45,10 @@ defmodule Remotex.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.5", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.1", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.14.3", only: [:dev, :test]}
     ]
   end
 
@@ -54,10 +60,38 @@ defmodule Remotex.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
+      c: "compile",
+      co: "compile --all-warnings --ignore-module-conflict --debug-info",
+      "format.all": [
+        "format mix.exs 'lib/**/*.{ex,exs}' 'test/**/*.{ex,exs}' 'config/*.{ex,exs}' 'priv/**/*.{ex,exs}'"
+      ],
+      check: ["credo --strict", "dialyzer"],
+      "format.check": [
+        "format --check-formatted mix.exs 'lib/**/*.{ex,exs}' 'test/**/*.{ex,exs}' 'config/*.{ex, exs}' 'priv/**/*.{ex,exs}'"
+      ],
+      cover: "coveralls",
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+    ]
+  end
+
+  defp dialyzer do
+    [
+      ignore_warnings: ".dialyzer_ignore.exs",
+      plt_add_apps: [:ex_unit, :jason, :mix],
+      plt_add_deps: :app_tree,
+      plt_file: {:no_warn, "priv/plts/remotex_1_12_2_otp_24.plt"}
+    ]
+  end
+
+  defp preferred_cli_env do
+    [
+      coveralls: :test,
+      cover: :test,
+      "cover.detail": :test,
+      "cover.html": :test
     ]
   end
 end
